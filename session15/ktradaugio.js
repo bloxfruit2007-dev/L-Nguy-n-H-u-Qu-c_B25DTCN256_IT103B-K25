01cf168d420f1971f29f4c7312557fbc3bede3d4
lenguyenhuuquoc <bloxfruit2007@gmail.com>
2026-03-26 13:13:33 +0700

    Add files via upload

--- session15/ktradaugio.js
+++ session15/ktradaugio.js
@@ -3,14 +3,5 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 
-form.addEventListener("submit", function(event) {
-    const emailValue = email.value;
-    const passwordValue = password.value;
-    const confirmPasswordValue = confirmPassword.value;
-
-    if(passwordValue !== confirmPasswordValue){
-        alert("Mật khẩu nhập lại không khớp!");
-        return;
-    }
-    alert("Đăng ký thành công!");
-});
+form.addEventListener("submit", function(event)) {
+}
